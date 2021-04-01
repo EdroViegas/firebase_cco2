aacc94f17b54d0e2dbc9869eac3c1a2717b1f311
EdroViegas <pedropombalviegas@gmail.com>
2021-04-01 01:06:28 +0100

    Added responsivity with responsive_framework

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,19 @@ class MyApp extends StatelessWidget {
                   context.read<AuthenticationService>().authStateChanges)
         ],
         child: MaterialApp(
+          builder: (context, widget) => ResponsiveWrapper.builder(
+              BouncingScrollWrapper.builder(context, widget),
+              maxWidth: 1200,
+              minWidth: 450,
+              defaultScale: true,
+              breakpoints: [
+                ResponsiveBreakpoint.resize(450, name: MOBILE),
+                ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+                ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+                ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+              ],
+              background: Container(color: Color(0xFFF5F5F5))),
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
